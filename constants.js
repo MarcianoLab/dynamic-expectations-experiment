bbd5ed3fad82034f2927659abea8490863dc4290
trapkey13 <trapkey13@gmail.com>
2025-10-13 13:53:58 +0300

    amazing
    amazing

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
-const GAME_1 = { id: "game1", diceResults: [2, 2, 2, 6, 5, 3] };
-const GAME_2 = { id: "game2", diceResults: [4, 5, 5, 1, 2, 3] };
-const GAME_3 = { id: "game3", diceResults: [5, 3, 3, 4, 1, 6] };
-const GAME_4 = { id: "game4", diceResults: [1, 4, 4, 3, 4, 6] };
-const GAME_5 = { id: "game5", diceResults: [6, 3, 5, 1, 4, 5] };
-const PRACTICE_GAME = { id: "practice", diceResults: [6, 1, 3, 4, 1, 2] };
+const GAME_1 = { id: "game1", diceResults: [1, 1, 1, 1, 1, 1] };
+const GAME_2 = { id: "game2", diceResults: [1, 1, 1, 1, 1, 1] };
+const GAME_3 = { id: "game3", diceResults: [1, 1, 1, 1, 1, 1] };
+const GAME_4 = { id: "game4", diceResults: [1, 1, 1, 1, 1, 1] };
+const GAME_5 = { id: "game5", diceResults: [1, 1, 1, 1, 1, 1] };
+const PRACTICE_GAME = { id: "practice", diceResults: [1, 1, 1, 1, 1, 1] };
 const PREPARED_GAME_LIST = [GAME_1, GAME_2, GAME_3, GAME_4, GAME_5];
